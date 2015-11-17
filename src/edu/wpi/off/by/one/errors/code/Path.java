e1f785b235b61e1918e66f253c0554ba76024548
David Rubenstein <david.p.rubenstein@gmail.com>
2015-11-16 19:27:43 -0500

    Added tests folder/file and the beginnings of path management

--- src/edu/wpi/off/by/one/errors/code/Path.java
+++ src/edu/wpi/off/by/one/errors/code/Path.java
@@ -1,5 +1,39 @@
 package edu.wpi.off.by.one.errors.code;
 
-public class Path {
+import java.util.ArrayList;
 
+public class Path {
+	private int startNode;
+	private int endNode;
+	private int[] route;
+	private Node[] nodes;
+	private Edge[] edges;
+	
+	public Path(int startNodeIn, int endNodeIn){
+		startNode = startNodeIn;
+		endNode = endNodeIn;
+	}
+	public void runAStar(Node[] nodesIn, Edge[] edgesIn){
+		ArrayList<Integer> visited = new ArrayList<Integer>();
+		ArrayList<Integer> toVisit = new ArrayList<Integer>();
+		nodes = nodesIn;
+		edges = edgesIn;
+		
+		
+	}
+	public void setStartNode(int startNodeIn){
+		startNode = startNodeIn;
+	}
+	public void setEndNode(int endNodeIn){
+		endNode = endNodeIn;
+	}
+	public int getStartNode(){
+		return startNode;
+	}
+	public int getEndNode(){
+		return endNode;
+	}
+	public int[] getRoute(){
+		return route;
+	}
 }
