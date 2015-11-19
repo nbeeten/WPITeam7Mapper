747a4027aea58bf80992cad5517b6623cad37a9f
roboman2444 <roboman2444@gmail.com>
2015-11-19 02:04:39 -0500

    Updated IDs and the way graph handles lists of shit. Havent updated Path to reflect this change. This might break a lot of stuff

--- src/edu/wpi/off/by/one/errors/code/Graph.java
+++ src/edu/wpi/off/by/one/errors/code/Graph.java
@@ -4,12 +4,20 @@ import java.util.*;
 
 //graph class manages edges and nodes
 public class Graph {
+	private int node_count = 0;
+	private int node_arrayfirstopen= 0;
+	private int node_arraylasttaken = -1;
+	private int node_arraysize = 0;
 	private Vector<Node> listOfNodes;
+	private int edge_count = 0;
+	private int edge_arrayfirstopen= 0;
+	private int edge_arraylasttaken = -1;
+	private int edge_arraysize = 0;
 	private Vector<Edge> listOfEdges;
 
 	public Graph(){
-	listOfNodes = new Vector<Node>(); //array list of nodes
-	listOfEdges = new Vector<Edge>(); //array list of edges
+		listOfNodes = new Vector<Node>(); //array list of nodes
+		listOfEdges = new Vector<Edge>(); //array list of edges
 	}
 	
 	/**
@@ -19,9 +27,17 @@ public class Graph {
 	 */
 
 	public Node addNode(Coordinate coordIn){//adds a node to the list
-		Node n = new Node(coordIn);
-		n.setId(listOfNodes.size());
-		listOfNodes.add(n);
+		node_count++;
+		for(; node_arrayfirstopen < node_arraysize && listOfNodes.get(node_arrayfirstopen) != null; node_arrayfirstopen++);
+		if(node_arrayfirstopen == node_arraysize){	//resize
+			node_arraysize++;
+			listOfNodes.ensureCapacity(node_arraysize);
+		}
+		Id nid = new Id(node_arrayfirstopen, node_count);
+		Node n = new Node(coordIn, nid);
+		listOfNodes.set(node_arrayfirstopen, n);
+		
+		if(node_arraylasttaken < node_arrayfirstopen) node_arraylasttaken = node_arrayfirstopen; //todo redo
 		return n;
 	}
 	
@@ -30,12 +46,19 @@ public class Graph {
 	 * @param coordIn
 	 * @return node's id 
 	 */
-	public int addNodeRint(Coordinate coordIn){//adds a node to the list, returns ID instead of node
-		Node n = new Node(coordIn);
-		int id = listOfNodes.size();
-		n.setId(id);
-		listOfNodes.add(n);
-		return id;
+	public Id addNodeRint(Coordinate coordIn){//adds a node to the list, returns ID instead of node
+		node_count++;
+		for(; node_arrayfirstopen < node_arraysize && listOfNodes.get(node_arrayfirstopen) != null; node_arrayfirstopen++);
+		if(node_arrayfirstopen == node_arraysize){	//resize
+			node_arraysize++;
+			listOfNodes.ensureCapacity(node_arraysize);
+		}
+		Id nid = new Id(node_arrayfirstopen, node_count);
+		Node n = new Node(coordIn, nid);
+		listOfNodes.set(node_arrayfirstopen, n);
+		
+		if(node_arraylasttaken < node_arrayfirstopen) node_arraylasttaken = node_arrayfirstopen; //todo redo
+		return nid;
 	}
 	
 	/**
@@ -43,10 +66,12 @@ public class Graph {
 	 * @param id
 	 * @return node that matches the id; null if couldn't find it
 	 */
-	public Node returnNodeById(int id){
-		Node n = listOfNodes.elementAt(id);
-		if(n != null && n.getId() == id) return n;
-		return null;
+	public Node returnNodeById(Id id){
+		Node n = listOfNodes.elementAt(id.indice);
+		if(n == null) return null;
+		Id nid = n.getId();
+		if(nid.indice != id.indice || nid.unique != id.unique) return null; 
+		return n;
 	}
 	
 	/**
@@ -55,14 +80,25 @@ public class Graph {
 	 * @param nodeBIn
 	 * @return added edge
 	 */
-	public Edge addEdge(int nodeAIn, int nodeBIn){//adds an edge to the list
-		Edge e = new Edge(nodeAIn, nodeBIn); 
-		int id = listOfEdges.size();
-		e.setId(id);
-		listOfEdges.add(e);
-		listOfNodes.get(nodeAIn).addEdgeId(id);
-		listOfNodes.get(nodeBIn).addEdgeId(id);
-		e.updateLength((float) Math.sqrt(Math.pow((listOfNodes.get(nodeAIn).getCoordinate().getX()-listOfNodes.get(nodeBIn).getCoordinate().getX()), 2)+Math.pow((listOfNodes.get(nodeAIn).getCoordinate().getY()-listOfNodes.get(nodeBIn).getCoordinate().getY()), 2)+Math.pow((listOfNodes.get(nodeAIn).getCoordinate().getZ()-listOfNodes.get(nodeBIn).getCoordinate().getZ()), 2)));
+	public Edge addEdge(Id nodeAIn, Id nodeBIn){//adds an edge to the list
+		Node A = returnNodeById(nodeAIn);
+		Node B = returnNodeById(nodeBIn);
+		if(A == null || B == null) return null;
+		edge_count++;
+		for(; edge_arrayfirstopen < edge_arraysize && listOfEdges.get(edge_arrayfirstopen) != null; edge_arrayfirstopen++);
+		if(edge_arrayfirstopen == edge_arraysize){	//resize
+			edge_arraysize++;
+			listOfEdges.ensureCapacity(edge_arraysize);
+		}
+		Id eid = new Id(edge_arrayfirstopen, edge_count);
+		Edge e = new Edge(nodeAIn, nodeBIn, eid);
+		listOfEdges.set(edge_arrayfirstopen, e);
+		
+		if(edge_arraylasttaken < edge_arrayfirstopen) edge_arraylasttaken = edge_arrayfirstopen; //todo redo
+
+		A.addEdgeId(eid);
+		B.addEdgeId(eid);
+		e.updateLength((float) Math.sqrt(Math.pow((A.getCoordinate().getX()-B.getCoordinate().getX()), 2)+Math.pow((A.getCoordinate().getY()-B.getCoordinate().getY()), 2)+Math.pow((A.getCoordinate().getZ()-B.getCoordinate().getZ()), 2)));
 		return e;
 	}
 	/**
@@ -71,18 +107,26 @@ public class Graph {
 	 * @param nodeBIn
 	 * @return id of added edge
 	 */
-	public int addEdgeRint(int nodeAIn, int nodeBIn){//adds an edge to the list, returns ID instead of edge
-		Edge e = new Edge(nodeAIn, nodeBIn); 
-		int id = this.listOfEdges.size();
-		e.setId(id);
-		this.listOfEdges.add(e);
-		listOfNodes.get(nodeAIn).addEdgeId(id);
-		listOfNodes.get(nodeBIn).addEdgeId(id);
-		float xDist = listOfNodes.get(nodeAIn).getCoordinate().getX()-listOfNodes.get(nodeBIn).getCoordinate().getX();
-		float yDist = listOfNodes.get(nodeAIn).getCoordinate().getY()-listOfNodes.get(nodeBIn).getCoordinate().getY();
-		float zDist = listOfNodes.get(nodeAIn).getCoordinate().getZ()-listOfNodes.get(nodeBIn).getCoordinate().getZ();
-		e.updateLength((float) Math.sqrt(xDist*xDist+yDist*yDist+zDist*zDist));
-		return id;
+	public Id addEdgeRint(Id nodeAIn, Id nodeBIn){//adds an edge to the list, returns ID instead of edge
+		Node A = returnNodeById(nodeAIn);
+		Node B = returnNodeById(nodeBIn);
+		if(A == null || B == null) return null;
+		edge_count++;
+		for(; edge_arrayfirstopen < edge_arraysize && listOfEdges.get(edge_arrayfirstopen) != null; edge_arrayfirstopen++);
+		if(edge_arrayfirstopen == edge_arraysize){	//resize
+			edge_arraysize++;
+			listOfEdges.ensureCapacity(edge_arraysize);
+		}
+		Id eid = new Id(edge_arrayfirstopen, edge_count);
+		Edge e = new Edge(nodeAIn, nodeBIn, eid);
+		listOfEdges.set(edge_arrayfirstopen, e);
+		
+		if(edge_arraylasttaken < edge_arrayfirstopen) edge_arraylasttaken = edge_arrayfirstopen; //todo redo
+
+		A.addEdgeId(eid);
+		B.addEdgeId(eid);
+		e.updateLength((float) Math.sqrt(Math.pow((A.getCoordinate().getX()-B.getCoordinate().getX()), 2)+Math.pow((A.getCoordinate().getY()-B.getCoordinate().getY()), 2)+Math.pow((A.getCoordinate().getZ()-B.getCoordinate().getZ()), 2)));
+		return eid;
 	}
 	
 	/**
@@ -90,14 +134,12 @@ public class Graph {
 	 * @param id
 	 * @return edge that matches the id; null if couldn't find it
 	 */
-	public Edge returnEdgeById(int id){
-		System.out.println("start of return edge by ID");
-		System.out.println(id);
-		System.out.println(listOfEdges.size());
-		System.out.println("end of returnedgebyID printing");
-		Edge e = listOfEdges.elementAt(id);
-		if(e != null && e.getId() == id) return e;
-		return null;
+	public Edge returnEdgeById(Id id){
+		Edge e = listOfEdges.elementAt(id.indice);
+		if(e == null) return null;
+		Id eid = e.getId();
+		if(eid.indice != id.indice || eid.unique != id.unique) return null; 
+		return e;
 	}
 	
 	public Vector<Node> getNodes(){//gets the list of nodes
@@ -112,20 +154,26 @@ public class Graph {
 	 * delete node
 	 * @param target
 	 */
-	public void deleteNode(int target){//deletes a node from the list (replaces it with null)
-		Node n = listOfNodes.elementAt(target);
-		if(n != null && n.getId() == target) listOfNodes.set(target, null);
-		//will change this when we upgrade ids
+	public boolean deleteNode(Id target){//deletes a node from the list (replaces it with null)
+		Node n = returnNodeById(target);
+		if(n == null) return false;
+		listOfNodes.set(target.indice, null);
+		if(target.indice< node_arrayfirstopen) node_arrayfirstopen = target.indice;
+		for(; node_arraylasttaken > 0 && listOfNodes.get(node_arraylasttaken) == null; node_arraylasttaken--);
+		return true;
 	}
 	
 	/**
 	 * delete edge
 	 * @param target
 	 */
-	public void deleteEdge(int target){//deletes an edge from the list (replaces it with null)
-		Edge e = listOfEdges.elementAt(target);
-		if(e != null && e.getId() == target) listOfEdges.set(target, null);
-		//will change this when we upgrade ids
+	public boolean deleteEdge(Id target){//deletes an edge from the list (replaces it with null)
+		Edge e = returnEdgeById(target);
+		if(e == null) return false;
+		listOfEdges.set(target.indice, null);
+		if(target.indice< edge_arrayfirstopen) edge_arrayfirstopen = target.indice;
+		for(; edge_arraylasttaken > 0 && listOfEdges.get(edge_arraylasttaken) == null; edge_arraylasttaken--);
+		return true;
 	}
 	
 	/**
@@ -133,13 +181,12 @@ public class Graph {
 	 * @param target
 	 * @param newCoord
 	 */
-	public void editNode(int target, Coordinate newCoord){//edits a node on the list
-		Node n = listOfNodes.elementAt(target);
-		if(n != null && n.getId() == target){
+	public void editNode(Id target, Coordinate newCoord){//edits a node on the list
+		Node n = returnNodeById(target);
+		if(n != null){
 			n.setCoordinate(newCoord);
-			listOfNodes.set(target, n);
+			listOfNodes.set(target.indice, n);
 		}
-		//will change this when we upgrade ids
 	}
 	
 	public void editEdge(){//edits an edge on the list
