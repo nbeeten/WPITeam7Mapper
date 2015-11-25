d887a7e157b7671b9acb94ead63cbcfa61da9f0f
Jules Voltaire <javoltaire@wpi.edu>
2015-11-25 12:58:14 -0500

    Added Constructor for SearchLocationMeny

--- src/edu/wpi/off/by/one/errors/code/controller/SearchLocationMenuController.java
+++ src/edu/wpi/off/by/one/errors/code/controller/SearchLocationMenuController.java
@@ -1,16 +1,30 @@
 package edu.wpi.off.by.one.errors.code.controller;
 
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.scene.layout.*;
+
+import javax.imageio.IIOException;
 
 /**
  * Created by Jules on 11/22/2015.
  */
-public class SearchLocationMenuController implements Initializable{
+public class SearchLocationMenuController extends AnchorPane{
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
 
+    public SearchLocationMenuController(){
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/SearchLocationMenuView.fxml"));
+        loader.setRoot(this);
+        loader.setController(this);
+
+        try{
+            loader.load();
+        }catch (IOException excpt){
+            throw new RuntimeException(excpt);
+        }
     }
 }
