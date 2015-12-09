c5e167ebfd15e293111b4cb04df871d5131f2006
Jules Voltaire <javoltaire@wpi.edu>
2015-12-09 01:11:55 -0500

    Added javax.mail jar file

--- src/edu/wpi/off/by/one/errors/code/controller/menupanes/SettingsMenuPane.java
+++ src/edu/wpi/off/by/one/errors/code/controller/menupanes/SettingsMenuPane.java
@@ -1,5 +1,6 @@
 package edu.wpi.off.by.one.errors.code.controller.menupanes;
 
+import java.io.FileWriter;
 import java.io.IOException;
 
 import edu.wpi.off.by.one.errors.code.controller.MainPane;
@@ -10,7 +11,29 @@ import javafx.fxml.FXMLLoader;
  * Created by jules on 11/28/2015.
  */
 public class SettingsMenuPane extends BorderPane {
+    //region Attributes
+    //region Constants for Identify each line when save/loading user info from textfile.
+    private final String NAME_STRING = "USER_NAME";
+    private final String PHONE_STRING = "USER_PHONE";
+    private final String EMAIL_STRING = "USER_EMAIL";
+    //endregion
 
+    //region FXML attributes
+
+    //endregion
+
+    //region Other attributes
+    private final String FILE_LOCATION = "@../../resources/UserSettings.txt";
+    private FileWriter writer;
+    
+    //endregion
+    //endregion
+
+    //region Constructors
+    /**
+     * Constructor
+     * Will load the fxml file and initialize necessary controls and layouts
+     */
     public SettingsMenuPane(){
         FXMLLoader loader = new FXMLLoader(getClass().getResource("../../view/menupanes/SettingsMenuPane.fxml"));
 
@@ -22,9 +45,6 @@ public class SettingsMenuPane extends BorderPane {
             throw new RuntimeException(excpt);
         }
     }
+    //endregion
 
-	public void setMainPane(MainPane mainPane) {
-		// TODO Auto-generated method stub
-		
-	}
 }
